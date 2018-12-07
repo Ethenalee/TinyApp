80d7dd606c7afb8936cb38cb669e72618ed84570
Ethena lee <yeonji.900111@gmail.com>
2018-12-07 03:58:48 +0000

    make comment and clean codes

--- express_server.js
+++ express_server.js
@@ -2,12 +2,13 @@ let express = require('express');
 let app = express();
 let PORT = 8080; // default port 8080
 
+// require list
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
 
 
-
+// using require
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(
@@ -20,12 +21,13 @@ app.use(
 );
 
 
-
+// main database
 var urlDatabase = {
   'b2xVn2': {longURL: 'http://www.lighthouselabs.ca', user:'asdfsdf'},
   '9sm5xK': {longURL: 'http://www.google.com', user:'asdfsdf'}
 };
 
+// user database
 const users = {
   asdfsdf: {
       id: 'asdfsdf',
@@ -35,6 +37,7 @@ const users = {
 
 };
 
+// random ID generate for server purpose
 function generateRandomString(length) {
   let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -44,6 +47,7 @@ function generateRandomString(length) {
     return text;
 }
 
+// bring ID matching with email
 function bringID(email) {
   let [userId] = Object.keys(users).filter(
     userid =>
@@ -52,7 +56,7 @@ function bringID(email) {
   return userId;
 }
 
-
+// matching existing ID
 function userthere(email) {
   for (let userId in users) {
     if(users[userId].email === email) {
@@ -62,6 +66,7 @@ function userthere(email) {
   return false;
 }
 
+// generate object for each users urls
 function urlsForUser(id) {
   let urlslist = {}
   for (let shork in urlDatabase) {
@@ -74,24 +79,44 @@ function urlsForUser(id) {
 
 }
 
-
+// main page
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  let user_id = req.session.user_id;
+
+  if(user_id) {
+    res.redirect('/urls')
+  }
+  else {
+    res.redirect('/login');
+  }
+
 });
 
+// hello page
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+// shorturl page can redirect to long url website when they are logged in
 app.get('/u/:shortURL', (req, res) => {
+  let user_id = req.session.user_id;
   let longURL = urlDatabase[req.params.shortURL].longURL;
+
+  if(user_id) {
   res.redirect(longURL)
+  }
+  else {
+    res.render('permission');
+  }
+
 });
 
+// can check the main database;
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+// page that can show all the urls you created
 app.get('/urls', (req, res) => {
   let user_id = req.session.user_id;
   let templateVars = {
@@ -108,11 +133,12 @@ app.get('/urls', (req, res) => {
   }
 });
 
+// page that you can create new url list
 app.get('/urls/new', (req, res) => {
   let user_id = req.session.user_id;
   let templateVars = {
     users,
-    user_id: req.session.user_id
+    user_id
   };
 
   if(user_id) {
@@ -123,6 +149,7 @@ app.get('/urls/new', (req, res) => {
   }
 });
 
+// generates a short URL, saves it, and associates it with the user
 app.post('/urls', (req, res) => {
   let shorK = generateRandomString(6);
 
@@ -134,6 +161,8 @@ app.post('/urls', (req, res) => {
   res.redirect('/urls/'+ shorK);
 
 });
+
+//see owns the URL for the given ID
 
 app.get('/urls/:id', (req, res) => {
   let shortURL = req.params.id;
@@ -148,24 +177,35 @@ app.get('/urls/:id', (req, res) => {
   }
 });
 
+
+// delete url process
 app.post('/urls/:id/delete', (req, res) => {
   let shorK = req.params.id;
   let user_id = req.session.user_id;
-  if(urlDatabase[shorK].user === user_id) {
-    delete urlDatabase[shorK];
-    res.redirect('/urls/');
+
+  if(user_id) {
+      if (urlDatabase[shorK].user === user_id) {
+          delete urlDatabase[shorK];
+          res.redirect('/urls/');
+        }
+      else {
+        res.send('You do not have a permission to delete');
+      }
   }
   else {
-    res.send('You do not have a permission to delete');
+    res.send('You do not have a permission to delete')
   }
+
 });
 
+// updates the URL
 app.post('/urls/:id', (req, res) => {
   let shorK = req.params.id;
 
   res.redirect('/urls/');
 });
 
+// login page
 app.get('/login', (req, res) => {
   let templateVars = {
     users,
@@ -175,37 +215,38 @@ app.get('/login', (req, res) => {
   res.render('login', templateVars);
 });
 
+// sets a cookie
 app.post('/login', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   let id = bringID(email);
-  ;
-
 
   if (!id) {
     res.status(403).send('Error 403: Email is not valid');
   }
-
   else if (!bcrypt.compareSync(password, users[id].password)) {
     res.status(403).send('Error 403: password is not valid');
   }
   else {
     req.session.user_id = id;
-    res.redirect('/');
+    res.redirect('/urls');
   }
 
 });
 
+// delete cookies
 app.post('/logout', (req, res) => {
   req.session = null;
-  res.redirect('/urls/');
+  res.redirect('/urls');
 });
 
+//register page
 app.get('/register', (req, res) => {
   let templateVars = { users, user_id: req.session.user_id};
   res.render('urls_email', templateVars);
 });
 
+//create new user
 app.post('/register', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
@@ -226,14 +267,13 @@ app.post('/register', (req, res) => {
       email: email,
       password: hashedPassword
     };
-
     req.session.user_id = id;
     res.redirect('/urls/');
   }
 
 });
 
-
+//show which port running now
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
